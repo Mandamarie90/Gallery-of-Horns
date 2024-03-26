5bfec16b99c1ec081675bea081422e6db595b2e2
Amanda <amandamarquez27@icloud.com>
2024-03-25 23:00:45 -0700

    Starting react with components

---
+++ Gallery of Horns/src/components/Gallery.jsx
@@ -0,0 +1,32 @@
+// import HornedBeast from './HornedBeast.js';
+
+// function Gallery() {
+
+//   const hornedBeastsData = [
+//     {
+//       title: "Elephant",
+//       imageUrl: "elephant.jpg",
+//       description: "The largest land animal on Earth."
+//     },
+//     {
+//       title: "Lion",
+//       imageUrl: "lion.jpg",
+//       description: "The king of the jungle."
+//     }
+//   ];
+
+//   return (
+//     <div>
+//       {hornedBeastsData.map((beast, index) => (
+//         <HornedBeast
+//           key={index}
+//           title={beast.title}
+//           imageUrl={beast.imageUrl}
+//           description={beast.description}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Gallery;
